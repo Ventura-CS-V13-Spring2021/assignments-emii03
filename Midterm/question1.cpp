a7c2183db15d41781b659003360a5a8d0cb4092e
Emily Rosales <>
2021-04-28 00:43:15 +0000

    added in a while loop

--- Midterm/question1.cpp
+++ Midterm/question1.cpp
@@ -6,17 +6,29 @@ using namespace std;
 
 int main() {
 
-  string Name;
-  int score1, score2;
 
   ifstream inFile;
 
-  inFile.open("students.txt");
+  string name;
+
+  int score1, score2;
+  int sum;
+  int average;
+
+
+
+  inFile.open("students.txt");//to allow interaction with txt file
 
   if (inFile.fail()){
     cout <<"Error" << endl;
     exit (0);
-  
+
+
+  }
+
+  while (inFile.good()){
+    cout << "Student:" << name << "\t";
+    cout << "Score 1:" << score1 << "\t" << "Score 2:" << score2 << "\t";
 
   }
 
