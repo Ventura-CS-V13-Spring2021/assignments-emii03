3d4fb1157ef5ff9bf74b948915a808582954fce6
Emily Rosales <>
2021-04-28 01:25:29 +0000

    started question 2

--- Midterm/question1.cpp
+++ Midterm/question1.cpp
@@ -20,7 +20,7 @@ int main() {
   inFile.open("students.txt");//to allow interaction with txt file
 
   if (inFile.fail()){
-    cout <<"Error" << endl;
+    cout <<"File error" << endl;
     exit (0);
 
 
@@ -29,6 +29,20 @@ int main() {
   while (inFile.good()){
     cout << "Student:" << name << "\t";
     cout << "Score 1:" << score1 << "\t" << "Score 2:" << score2 << "\t";
+   
+   sum = score1 + score2;//
+   cout << "Sum:" << sum << "\t";
+
+   average = sum / 2;
+   cout << "Average:" << average << "\t";
+
+   if (average >= 80)
+   {
+     
+   }
+
+
+
 
   }
 
---
+++ Midterm/question2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int getRdnum(void);
+int isGreater(int);
+
+int main(){
+
+  int N;
+  
+  ofstream ofs;
+
+  ofs.open("numbers.txt");
+  if (ofs.fail())
+  {
+    cout << "File open error" << endl;
+    exit(0);
+
+  }
+  for (int i = 0; i <10; i++)
+{
+  
+}
+}
+
